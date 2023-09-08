8d21b2c746560e9ea8e98eb427e82d75f47c91f6
wangzd <you@example.com>
2023-09-08 01:49:18 +0000

    update

--- main.go
+++ main.go
@@ -3,14 +3,16 @@ package main
 import (
                 "fmt"
                 "crypto/tls"
-                amberclient "github.com/intel/amber-client/go-client"
+                //amberclient "github.com/intel/amber-client/go-client"
+                amberclient "github.com/intel/amber/v1/client"
                 "github.com/intel/amber/v1/client/tdx"
                 "log"
+_		"github.com/google/uuid"
         )
 
 func main(){
         cfg:= amberclient.Config{
-                BaseUrl: "https://api.projectamberla.intel.com/",
+                BaseUrl: "https://projectamberla.intel.com",
                 ApiUrl: "https://api.projectamberla.intel.com",
                 TlsCfg: &tls.Config{},
                 ApiKey: "MnbKbiZy5T8Okyvc28SCDAm2dCouoF28LTJsTMR4",
@@ -29,11 +31,37 @@ func main(){
                 log.Println(err)
                 return
         }
-	tdxnonce:=make([]byte, 10, 10)
+	/*tdxnonce:=make([]byte, 10, 10)
         evidence, err := adapter.CollectEvidence(tdxnonce)
         if err != nil {
                 log.Println(err)
                 return
         }
-        log.Println(evidence)
+
+        log.Println(evidence)*/
+	/*ids := make([]uuid.UUID,1)
+	token, err := client.GetToken(nonce, ids, evidence)
+	if err != nil {
+    		fmt.Printf("Something bad happened: %s\n\n", err)
+    		return 
+	}
+        log.Println(token)*/
+	token, err := client.CollectToken(adapter, nil)
+	if err != nil {
+    		fmt.Printf("Something bad happened: %s\n\n", err)
+    		return 
+	}
+        log.Println(token)
+	jwks, err := client.GetAmberCertificates()
+	if err != nil {
+    		fmt.Printf("Something bad happened: %s\n\n", err)
+    		return 
+	}
+        log.Println(jwks)
+	parsedToken, err := client.VerifyToken(string(token))
+	if err != nil {
+    		fmt.Printf("Something bad happened: %s\n\n", err)
+    		return 
+	}
+	fmt.Println("verify token.", parsedToken)
 }
